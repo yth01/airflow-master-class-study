7a75ac0ed39fa1da73124b9c34aab73655562831
yth01 <yth011026@gmail.com>
2024-01-18 03:54:40 +0900

    feat: dags_bash_operator.py 추가

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,22 @@
+import pendulum
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 1, 17, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    bash_t1 >> bash_t2
